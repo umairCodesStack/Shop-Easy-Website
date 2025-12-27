c67759009fbb07e0135395673d1ad7cd8bcf2eb5
Umair Ashraf <umairjee360@gmail.com>
2025-12-27 22:10:13 +0500

    frontend removed

--- ShopEasy/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ShopEasy.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
